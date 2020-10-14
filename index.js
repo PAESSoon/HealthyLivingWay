29977b02ed07cf962881735c2841b98b3ee510da
Pamela S-S <atheanae@gmail.com>
2020-10-14 11:22:51 -0400

    delete unnecessary files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-$(function(){
-    $(document).scroll(function(){
-      var $nav = $("#nav-bar");
-      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#nav-bar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
-  });
+});
